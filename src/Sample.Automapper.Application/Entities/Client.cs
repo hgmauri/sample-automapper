9d5ca1efe14d3c6ef53a9df08cfbd0958d288785
Henrique Mauri <hgmauri@gmail.com>
2021-12-06 10:17:51 -0300

    private set Id

--- src/Sample.Automapper.Application/Entities/Client.cs
+++ src/Sample.Automapper.Application/Entities/Client.cs
@@ -5,7 +5,7 @@ namespace Sample.Automapper.Application.Entities
 {
     public class Client
     {
-        public virtual Guid Id { get; set; }
+        public virtual Guid Id { get; } = Guid.NewGuid();
         public virtual string? Name { get; set; }
         public virtual string? Email { get; set; }
         public virtual GenderType Gender { get; set; }
--- src/Sample.Automapper.Application/Entities/Group.cs
+++ src/Sample.Automapper.Application/Entities/Group.cs
@@ -3,7 +3,7 @@ namespace Sample.Automapper.Application.Entities;
 
 public class Group
 {
-    public virtual Guid Id { get; set; } = Guid.NewGuid();
+    public virtual Guid Id { get; } = Guid.NewGuid();
     public virtual DateTime CreatedAt { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
